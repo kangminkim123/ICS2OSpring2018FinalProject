b536fbed96070f07e429576150e83d59bfba8b08
Gillian <gillian.gonzales@ocsbstudent.ca>
2018-06-11 14:50:06 -0400

    added background sound

--- scene/level14.lua
+++ scene/level14.lua
@@ -70,7 +70,7 @@ function scene:create( event )
 		sword = audio.loadSound( sndDir .. "sword.mp3" ),
 		squish = audio.loadSound( sndDir .. "squish.mp3" ),
 		slime = audio.loadSound( sndDir .. "slime.mp3" ),
-		wind = audio.loadSound( sndDir .. "loops/DesertSound.mp3" ),
+		wind = audio.loadSound( sndDir .. "loops/WinterWind.mp3" ),
 		door = audio.loadSound( sndDir .. "door.mp3" ),
 		hurt = {
 			audio.loadSound( sndDir .. "hurt1.mp3" ),
